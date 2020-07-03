3a7486473bdfca9998238f42298def1dcecaadd4
Carlos <cpenas@ic-itcr.ac.cr>
2020-07-03 14:58:27 -0600

    abrir en navegador y limpiar archivos

--- indexador/src/aplicacion/Main.java
+++ indexador/src/aplicacion/Main.java
@@ -1,8 +1,15 @@
 package aplicacion;
 
+import java.awt.Desktop;
 import java.awt.EventQueue;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.StringTokenizer;
 
@@ -14,12 +21,60 @@ import Controller.PantallaController;
 import View.Pantalla;
 
 public class Main{
+	
+	public static void limpiarPaginas() {
+		File dir = new File(".\\");
+		  File[] directoryListing = dir.listFiles();
+		  if (directoryListing != null) {
+		    for (File child : directoryListing) {
+		      String[] extensiones = child.getAbsolutePath().split("\\.");
+		      String extension = extensiones[extensiones.length-1];
+		      if(extension.contentEquals("html")) {
+		    	  child.delete();
+		      }
+		    }
+		  }
+	}
+	
+	public static void abrirPagina(String titulo,String refs,String coleccion) throws IOException {
+		InputStream is = new FileInputStream(".\\archivos\\" + coleccion); 
+    	BufferedReader buf = new BufferedReader(new InputStreamReader(is)); 
+    	String line = buf.readLine(); 
+    	StringBuilder sb = new StringBuilder(); 
+    	while(line != null){ 
+    		sb.append(line).append("\n"); line = buf.readLine(); 
+    	} 
+    	String docToString = sb.toString();
+    	String[] paginasSeparadas = docToString.split("<html");
+    	for(int i=0; i < paginasSeparadas.length;i++) {
+    		paginasSeparadas[i] = "<html" + paginasSeparadas[i];
+    		String[] separarLineas = paginasSeparadas[i].split("\n");
+    		String lineaCache = "";
+    		for(int j=0; j < separarLineas.length-1;j++) {
+    			lineaCache += separarLineas[j]+"\n";
+    		}
+    		paginasSeparadas[i] = lineaCache;
+    	}
+    	String paginaBuscada = "";
+    	for(String pagina:paginasSeparadas) {
+    		if(FileAnalyzer.sacarTitle(pagina).equals(titulo+" ") && FileAnalyzer.sacarRefs(pagina).equals(refs)) {
+    			paginaBuscada = pagina;
+    			break;
+    		}
+    	}
+    	File htmlFile = new File(".\\" + titulo + ".html");
+    	FileWriter fileWriter = new FileWriter(titulo+".html");
+    	fileWriter.write(paginaBuscada);
+    	fileWriter.close();
+    	Desktop desktop = Desktop.getDesktop();
+    	desktop.open(htmlFile);
+	}
 
     public static void main(String[] args) throws IOException {
     	
+    	//abrirPagina("alon harazi wikipedia la enciclopedia libre","refs","wiki-p1.txt");
     	
-    	
-        FileAnalyzer.fillStopWords(); //crear la lista estatica de stopwords
+        /*FileAnalyzer.fillStopWords(); //crear la lista estatica de stopwords
         
     	String indexp1 = ".\\indexp1";//".\\index"
     	String indexp2 = ".\\indexp2";
